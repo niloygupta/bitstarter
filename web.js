4a204a5cba3d590c326caf642dccf4bce66b4911
niloygupta <niloygupta@192.168.0.100>
2013-07-07 23:14:34 +0530

    Blank line

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
+
 //var buf = new Buffer(64);
 app.get('/', function(request, response) {
  var buf =  fs.readFileSync('index.html');
