c6a63e8b4b000e9976baf2a6dd78c69ed6d2cb56
niloygupta <niloygupta@gmail.com>
2013-07-07 16:32:51 +0000

    Read from file

--- web.js
+++ web.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var fs = require('fs');
 var app = express.createServer(express.logger());
-
+//var buf = new Buffer(64);
 app.get('/', function(request, response) {
- var buf =  fs.readFileSync('./index.html');
+ var buf =  fs.readFileSync('index.html');
   response.send(buf.toString());
 });
 
