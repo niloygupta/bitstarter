ed9d0dad5940cbc51d040b281bb219595f92b0d5
niloygupta <niloygupta@192.168.0.100>
2013-07-08 09:31:13 +0530

    URL Reader

--- grader.js
+++ grader.js
@@ -24,6 +24,8 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
+var util = require('util');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -61,12 +63,27 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+var parseURL = function()
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-        .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_link>', 'Path to index.html', rest.get('http://fierce-reaches-1073.herokuapp.com').on('complete', function(result)
+        		{
+        			if (result instanceof Error) {
+        				console.error('Error: ' + util.format(response.message));
+        				}
+        			else {
+        					console.error("Wrote %s", "file.html");
+        					fs.writeFileSync("file.html", result);
+        				}
+        			};
+        		
+        		, HTMLFILE_DEFAULT)
+      //  .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+    
+    var checkJson = checkHtmlFile(program.url, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
